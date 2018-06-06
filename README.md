4edefee7de527eb8b1daa2d03b0af68cb916a53d
bertsss <robertobonifacio@creatinginfo.com>
2018-06-06 11:52:28 +0800

    Make controller function works

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 API Laravel Framework
+
+Step by step instruction
+-run composer install
+-create local database
+-edit your env file to connect your local database
+-run "php artisan migrate" to create tables from your database
+-run "php artisan db:seed --class=ProductsTableSeeder" for creating dump data
+-run "php artisan serve" to start your local project
--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -11,39 +11,32 @@ class ProductsController extends Controller
     //
     public function index()
     {
-        return response(Product::all())->header('Access-Control-Allow-Origin', '*');
+        return response(Product::all());
     }
- 
+
     public function show(Product $product)
     {
-        return response($product)->header('Access-Control-Allow-Origin', '*');
+        return response($product);
     }
- 
+
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|max:255',
-            'price' => 'integer',
-            'quantity' => 'integer',
-            'size' => 'max:255',
-            'code' => 'required|integer|unique:products',
-        ]);
         $product = Product::create($request->all());
- 
-        return response()->json($product, 201);
+
+        return response($product);
     }
- 
+
     public function update(Request $request, Product $product)
     {
         $product->update($request->all());
- 
-        return response($product)->header('Access-Control-Allow-Origin', '*');
+
+        return response($product);
     }
- 
+
     public function delete(Product $product)
     {
         $product->delete();
- 
-        return response()->json(null, 204);
+
+        return response(Product::all());
     }
 }
